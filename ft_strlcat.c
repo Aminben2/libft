4dd7d9e22b3313bd31db2c0bd1bcb9991144cbe8
Aminben2 <aminbenomar454@gmail.com>
2024-10-23 21:30:59 +0100

    libft-strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 20:31:30 by mbenomar          #+#    #+#             */
+/*   Updated: 2024/10/23 21:30:24 by mbenomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int ft_strlcat(char *dst, const char *src, unsigned int dsize)
+{
+    int dest_len;
+    int src_len;
+    int j;
+
+    src_len = ft_strlen(src);
+    dest_len = ft_strlen(dst);
+    if (dest_len == dsize)
+        return (src_len + dsize);
+    j = 0;
+    while (src[j] && j < dsize - dest_len - 1)
+    {
+        dst[dest_len + j] = src[j];
+        j++;
+    }
+    return (dest_len + src_len);
+}
