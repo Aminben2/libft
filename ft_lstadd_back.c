d3ecc89cf074d15babdc7db50fee8a10cd8a51db
Aminben2 <amibenomar454@gmail.com>
2024-10-27 08:57:26 +0100

    libft-lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 20:28:12 by mbenomar          #+#    #+#             */
-/*   Updated: 2024/10/26 20:48:13 by mbenomar         ###   ########.fr       */
+/*   Updated: 2024/10/27 08:56:53 by mbenomar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,24 @@ void ft_lstadd_back(t_list **lst, t_list *new)
     last_node->next = new;
 }
 
-int main(void)
-{
-    t_list *head = ft_lstnew("amine");
-        t_list *node1 = ft_lstnew("ben");
-            t_list *node2 = ft_lstnew("oma");
-                t_list *node3 = ft_lstnew("mohamed");
-    head->next = node1;
-    head->next->next = node2;
-    head->next->next->next = node3;
+// int main(void)
+// {
+//     t_list *head = ft_lstnew("amine");
+//         t_list *node1 = ft_lstnew("ben");
+//             t_list *node2 = ft_lstnew("oma");
+//                 t_list *node3 = ft_lstnew("mohamed");
+//     head->next = node1;
+//     head->next->next = node2;
+//     head->next->next->next = node3;
 
-    t_list *lst = head; 
-    ft_lstadd_back(lst,ft_lstnew("test"));
-    t_list *curr = head;
-    while (curr)
-    {
-        printf("%s\n",curr->content);
-        curr = curr->next;
-    }
+//     t_list **lst = &head; 
+//     ft_lstadd_back(lst,ft_lstnew("test"));
+//     t_list *curr = head;
+//     while (curr)
+//     {
+//         printf("%s\n",curr->content);
+//         curr = curr->next;
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
