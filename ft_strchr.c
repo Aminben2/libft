d18f3ee4b7fb45fb9b58004e68cf221a8388fce2
Aminben2 <amibenomar454@gmail.com>
2024-11-02 18:26:45 +0100

    done

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 21:32:34 by mbenomar          #+#    #+#             */
-/*   Updated: 2024/10/27 20:44:17 by mbenomar         ###   ########.fr       */
+/*   Updated: 2024/11/02 17:59:57 by mbenomar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strchr(const char *s, int c)
 	len = ft_strlen(s);
 	while (i <= len)
 	{
-		if (s[i] == (unsigned char)c)
+		if (s[i] == (char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
